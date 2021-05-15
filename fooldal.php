e8376d912773a0551e6bd715e32dc410cc6918ec
Ferenc1980 <hefike30@gmail.com>
2021-05-15 12:30:33 +0200

    Initial commit

---
+++ fooldal.php
@@ -0,0 +1,29 @@
+
+
+
+<style>
+    #kep{
+        float:right;
+    }
+    p{
+        font-style:justify;
+    }
+    body{
+        background-color:lightgrey;
+        border:2 solid black;
+        padding:2px;
+        
+        
+        
+    }
+    
+</style>
+<div class="container justify-content-center">
+<h1>A vitorlázásról</h1>
+<img src="vitorlas.jpg" alt="Vitorlas" title="Vitorlas" id="kep">
+<p>A Balatonon a római időktől kezdve úsznak hajók, melyeket sokáig leginkább halászatra használtak.  A balatoni hajózás első nagy korszaka a Festetics családhoz fűződik. A általuk üzemeltetett Kristóf nevű "sóhajó" teherszállító vitorlás volt. 1753-től szállított sót Balatonkeneséről Keszthelyre és visszafelé sem járt üresen. Fedélzetén fával, borral és egyéb áruval megrakva kötött ki Kenesén.</p>
+</div>
+<?php
+
+
+?>
